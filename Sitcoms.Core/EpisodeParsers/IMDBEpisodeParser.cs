e93f2a7509ddb9ed6a7391386d0eda214a9b6e15
Mateusz Włodarczyk <wlodarczyk.mateusz@gmail.com>
2017-02-12 00:11:22 +0100

    Fix compilation errors

--- Sitcoms.Core/EpisodeParsers/IMDBEpisodeParser.cs
+++ Sitcoms.Core/EpisodeParsers/IMDBEpisodeParser.cs
@@ -1,9 +1,9 @@
 ﻿using HtmlAgilityPack;
+using Sitcoms.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Sitcoms.Core.EpisodeParsers
 {
