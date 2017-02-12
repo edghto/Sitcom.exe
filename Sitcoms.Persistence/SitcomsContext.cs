ff5e878377a34a55aae46bb4ae683858eab714f1
Mateusz Włodarczyk <wlodarczyk.mateusz@gmail.com>
2017-02-12 12:59:13 +0100

    Create stub of Persistence library

---
+++ Sitcoms.Persistence/SitcomsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitcoms.Persistence
+{
+    class SitcomsContext : DbContext
+    {
+        public DbSet<Core.Models.Sitcom> Sitcoms { get; set; }
+        public DbSet<Core.Models.Season> Seasons { get; set; }
+        public DbSet<Core.Models.Episode> Episodes { get; set; }
+    }
+}
